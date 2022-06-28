283c051950c544abecce2a2f8d89973fb20a1a1d
Thibaut Monseigne <thibaut.monseigne@inria.fr>
2022-06-28 10:37:41 +0200

    :recycle: Reformat

--- connexion_base.php
+++ connexion_base.php
@@ -8,19 +8,19 @@ $config_base['nom_base']    = "p'titdom";
 
 // connexion à la base de données
 try {
-    $pdo = new PDO(	"mysql:host={$config_base['hote']};
-                    dbname={$config_base['nom_base']}", 
-                    "{$config_base['utilisateur']}", 
-                    "{$config_base['motdepasse']}");
-    
-    // afficher les messages d'erreurs pour trouver les erreurs
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-    
-    // jeu de caractères : UTF-8
-    $pdo->query("SET NAMES utf8");
-    $pdo->query("SET CHARACTER SET utf8");
+	$pdo = new PDO(
+		"mysql:host={$config_base['hote']};
+		dbname={$config_base['nom_base']}",
+		"{$config_base['utilisateur']}",
+		"{$config_base['motdepasse']}"
+	);
+
+	// afficher les messages d'erreurs pour trouver les erreurs
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+	// jeu de caractères : UTF-8
+	$pdo->query("SET NAMES utf8");
+	$pdo->query("SET CHARACTER SET utf8");
+} catch (PDOException $exception) {
+	echo "Connexion échouée : " . $exception->getMessage();
 }
-catch (PDOException $exception) {
-    echo "Connexion échouée : " . $exception->getMessage();
-}
-?>
